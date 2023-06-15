db1bc095a635fe5ddf9799d1837fb388ceb432cc
hovannhatautotest <19t1051013@husc.edu.vn>
2023-06-15 23:35:01 +0700

    web design by Ho Van Nhat

---
+++ Đăng nhập hệ thống_files/dangnhap.js
@@ -0,0 +1,23 @@
+﻿// validation form login
+const inputUsername = document.querySelector("#loginID");
+const inputPassword = document.querySelector("#password");
+const btnLogin = document.querySelector(".btn-custom");
+
+// validation form login
+
+btnLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (inputUsername.value === "" || inputPassword.value === "") 
+  {
+    alert("vui lòng không để trống");
+  } 
+  else if (inputUsername.value === "admin" && inputPassword.value === "admin") 
+  {
+    alert("Đăng nhập thành công!HiHi");
+    window.location.href = "trangchu.html";
+  }
+  else 
+  {  
+      alert("Đăng Nhập Thất Bại");
+  }
+});
